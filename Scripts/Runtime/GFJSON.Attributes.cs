fad14bf43e8b57c768bacd9bf2fd0af240c0b98f
Vedran Manojlovic <vmanojlo@gmail.com>
2025-11-02 04:13:33 +0100

    Allow usage of GFJSON attributes on properties too

--- Scripts/Runtime/GFJSON.Attributes.cs
+++ Scripts/Runtime/GFJSON.Attributes.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace NightRider.GFJSON
 {
-    [AttributeUsage( AttributeTargets.Field, AllowMultiple = false )]
+    [AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false )]
     public class JSONNameAttribute : Attribute
     {
         public string Name { get; set; }
         public JSONNameAttribute( string name ) { Name = name; }
     }
 
-    [AttributeUsage( AttributeTargets.Field, AllowMultiple = false )]
+    [AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false )]
     public class JSONSkipAttribute : Attribute { }
 }
